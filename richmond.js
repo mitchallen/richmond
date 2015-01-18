80f0098498166491ba6397008802953895702b02
mitchallen <mitch.c.allen@gmail.com>
2015-01-18 11:23:43 -0500

    Implemented richmond.js core functionality

--- richmond.js
+++ richmond.js
@@ -2,4 +2,75 @@
  * richmond.js
  */
 
-console.log("Hello World (richmond.js");
+var core = module.exports = {}; // For export
+
+core.mongoose = require( 'mongoose' ), 
+	Schema = core.mongoose.Schema, 
+	ObjectId = Schema.ObjectId,
+	_model = require('./lib/model'),
+	_dbConn = null;
+
+core.name    = require("./package").name;
+core.version = require("./package").version;
+
+core.model = function( name ) {
+	return _model.model( name.toLowerCase() );
+}
+
+core.addModel = function( modelName, model ) {
+	return _model.addModel( 
+			modelName, 
+			model, 
+			_dbConn  
+	);
+};
+
+core.closeConnection = function() {
+	
+	if( _dbConn ) {
+		core.mongoose.disconnect( 
+			function() {
+				// console.log( "DEBUG: closeConnection: mongoose.disconnect()" );
+			} 
+		);
+		_dbConn = null;
+	}
+}
+
+core.close = function() {
+
+	core.closeConnection();
+}
+
+core.connection = function() {
+	return _dbConn;
+}
+
+core.connect = function( connection, options ) {
+	
+	// TO BE SURE - issues?
+	core.closeConnection();
+	
+	if( ! connection ) {
+		throw new Error( ".connect() connection string not defined.")
+	}
+		
+	var cb = function( err ) {
+		if( err ) throw err;
+		// TODO - pooled connections
+		// NOTE: must use callback or may get errors reconnecting.
+	};
+		
+	if( ! options.user ) {
+		_dbConn = core.mongoose.createConnection( connection, cb );
+	} else {
+		if( ! options.pass ) {
+			throw new Error( "DB password not defined.");
+		}
+		_dbConn = core.mongoose.createConnection( connection, options, cb );
+	}
+	    
+    return this;
+};
+
+
---
+++ test/db-test.js
@@ -0,0 +1,119 @@
+/**
+ * File: db-test.js
+ */
+
+var request = require('supertest'),
+	should = require('should'),
+	// async = require('async'),
+	micro = require('../richmond'),
+	config = require('./test-config'),
+	getRandomInt = require('./test-lib').getRandomInt,
+	service   	= config.service,
+	port 	= process.env.MOCHA_TEST_PORT || 3021,
+	prefix 	= service.prefix,
+	connection = service.dbConn,
+	dbUser = service.dbUser,
+	dbPass = service.dbPass,
+	modelName = "RichmondDbTest";	// Will translate to lowercase
+
+describe('DB Tests', function () {
+	before(function () {
+		// ...
+	 });
+	
+	it( 'DB valid connection', function( done ) {
+		
+		var options = {
+				user: dbUser,
+				pass: dbPass
+		};
+		
+		micro.connect( connection, options );
+		var dbConn = micro.connection();
+		should.exist( dbConn );
+		micro.closeConnection();
+		done();
+	});
+	
+	it( 'DB connection is undefined', function( done ) {
+		
+		var options = {
+				user: dbUser,
+				pass: dbPass
+		};
+		
+		var tempConn = undefined;
+		try {
+			micro.connect( tempConn, options );
+			throw new Error( "connect() should have thrown an exception");
+		} catch( ex ) {
+			// Expected
+		}
+		
+		var dbConn = micro.connection();
+		should.not.exist( dbConn );
+		done();
+	});
+	
+	it( 'DB connection is null', function( done ) {
+		
+		var options = {
+				user: dbUser,
+				pass: dbPass
+		};
+		
+		var tempConn = null;
+		try {
+			micro.connect( tempConn, options );
+			throw new Error( "connect() should have thrown an exception");
+		} catch( ex ) {
+			// Expected
+		}
+		
+		var dbConn = micro.connection();
+		should.not.exist( dbConn );
+		done();
+	});
+	
+	it( 'DB password is undefined', function( done ) {
+		
+		var options = {
+				user: dbUser,
+				pass: undefined
+		};
+		
+		try {
+			micro.connect( connection, options );
+			throw new Error( "connect() should have thrown an exception");
+		} catch( ex ) {
+			// Expected
+		}
+		
+		var dbConn = micro.connection();
+		should.not.exist( dbConn );
+		done();
+	});
+	
+	it( 'DB password is null', function( done ) {
+		
+		var options = {
+				user: dbUser,
+				pass: null
+		};
+		
+		try {
+			micro.connect( connection, options );
+			throw new Error( "connect() should have thrown an exception");
+		} catch( ex ) {
+			// Expected
+		}
+		
+		var dbConn = micro.connection();
+		should.not.exist( dbConn );
+		done();
+	});
+	
+	after(function () {
+		micro.close();
+	});
+});
