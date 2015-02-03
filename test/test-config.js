c2721637f0d6ae1f717acb176421912b90be7937
mitchallen <mitch.c.allen@gmail.com>
2015-02-03 10:21:47 -0500

    ran test-config.js through jslint

--- test/test-config.js
+++ test/test-config.js
@@ -2,25 +2,22 @@
  * test-config.js
  */
 
+"use strict";
+
 var Controller = require('@minja/richmond-web-controller');
 
 module.exports = {
-		
-	controller: new Controller(),
-		
-	mongoose: {
-		uri:  process.env.TEST_MONGO_DB || 'mongodb://localhost/mytest',
-		user: process.env.TEST_MONGO_USER || null,
-		pass: process.env.TEST_MONGO_PASS || null	
-	},
-		
-	service: {
-		secret: process.env.APP_SECRET || null,
-		prefix: "/API",
-		port: process.env.TEST_PORT || null,
-		host: process.env.TEST_HOST || null,
-	    hostSsl:  process.env.TEST_SSL || null
-	}
+    controller: new Controller(),
+    mongoose: {
+        uri:  process.env.TEST_MONGO_DB || 'mongodb://localhost/mytest',
+        user: process.env.TEST_MONGO_USER || null,
+        pass: process.env.TEST_MONGO_PASS || null
+    },
+    service: {
+        secret: process.env.APP_SECRET || null,
+        prefix: "/API",
+        port: process.env.TEST_PORT || null,
+        host: process.env.TEST_HOST || null,
+        hostSsl:  process.env.TEST_SSL || null
+    }
 };
-
-
